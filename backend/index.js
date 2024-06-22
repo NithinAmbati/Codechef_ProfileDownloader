6dc01fe4dcaa4d8059db877c2ce8049c8e4749e1
Nithin Ambati <nithinambati@Nithins-MacBook-Air.local>
2024-06-22 11:45:16 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,26 @@ app.listen(8000, () => {
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running..." });
+});
+
+app.get("/codechef", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.codechef.com/users/nithin_ambati}`);
+    const screenshotBuffer = await page.screenshot({ fullPage: true });
+    browser.close();
+
+    const screenshotBase64 = screenshotBuffer.toString("base64");
+    res.status(200).json({ image: screenshotBase64 });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
 app.post("/codechef", async (req, res) => {
   const { username } = req.body;
   try {
